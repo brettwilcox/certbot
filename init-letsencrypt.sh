00ac7eb2a5f4947be03b50c186a66f233880db64
Brett Wilcox <brett@scorpion.io>
2020-11-12 03:49:50 -0600

    Updates

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -44,7 +44,7 @@ function make_dummy_certificate() {
   path="/etc/letsencrypt/live/${domains[0]}"
   mkdir -p "$data_path/conf/live/${domains[0]}"
   docker-compose run --rm --entrypoint " \
-    openssl req -x509 -nodes -newkey rsa:1024 -days 1 \
+    openssl req -x509 -nodes -newkey rsa:${rsa_key_size} -days 1 \
       -keyout '$path/privkey.pem' \
       -out '$path/fullchain.pem' \
       -subj '/CN=localhost'" certbot
